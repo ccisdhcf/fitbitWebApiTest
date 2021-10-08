87e7bd2a4ccbdb8c6e30a2407472835c5dcfbe81
DESKTOP-Q6BBGDK\wmlab <acd789456123@g.ncu.edu.tw>
2021-10-08 16:42:43 +0800

    challenge finish

---
+++ fitbitWebApiTest/Controllers/homeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fitbitWebApiTest.tools;
+
+namespace fitbitWebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class homeController : ControllerBase
+    {
+        private string strClientID = "23BKY4";
+        private string strClientSecret = "4177e6ef04511a2e6d94406c12ba8ccd";
+        private string strRedirectUrl = "https://google.com";
+        private readonly stringGen _stringGen = new();
+
+        [HttpGet]
+        public async Task< ActionResult> GetIndex()
+        {
+            string verifier = _stringGen.RandomString(43, 128);
+            string challenge = _stringGen.ComputeSha256Hash(verifier);
+            challenge = _stringGen.ComputeBase64Url(challenge);
+            return null;
+        }
+    }
+
+    
+}
+   
---
+++ fitbitWebApiTest/tools/stringGen.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitbitWebApiTest.tools
+{
+    public class stringGen
+    {
+        private static Random random = new Random();
+
+        public  string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public  string RandomString(int minLength, int maxLength)
+        {
+            int length = random.Next(minLength, maxLength + 1);
+            return RandomString(length);
+        }
+        public string ComputeSha256Hash(string text)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public string ComputeBase64Url(string text)
+        {
+            string result = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(text));
+     
+            return result;
+        }
+    }
+  
+
+  
+
+}
