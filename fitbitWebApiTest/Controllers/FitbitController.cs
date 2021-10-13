c7bab115eebd47af31b3498d07d36979c7e11398
DESKTOP-Q6BBGDK\wmlab <acd789456123@g.ncu.edu.tw>
2021-10-13 22:37:54 +0800

    api connect finish redirect OK need to merge react

--- fitbitWebApiTest/Controllers/FitbitController.cs
+++ fitbitWebApiTest/Controllers/FitbitController.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using fitbitWebApiTest.tools;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using fitbitWebApiTest.tools;
-using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace fitbitWebApiTest.Controllers
 {
-    [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
-    public class HomeController : ControllerBase
+    public class FitbitController : ControllerBase
     {
+
         private string strClientID = "23BKY4";
         private string strClientSecret = "4177e6ef04511a2e6d94406c12ba8ccd";
         private string strRedirectUrl = "https://google.com";
@@ -27,16 +27,14 @@ namespace fitbitWebApiTest.Controllers
         private string strAuthUrl = "https://www.fitbit.com/oauth2/authorize?client_id={0}&response_type={1}&code_challenge={2}&code_challenge_method={3}&scope={4}";
 
         [HttpGet]
-        public  ActionResult GetIndex()
+        public ActionResult Get()
         {
             strVerifier = _stringGen.RandomString(43, 128);
             strCodeChallenge = _stringGen.ComputeSha256Hash(strVerifier);
             strCodeChallenge = _stringGen.ComputeBase64Url(strCodeChallenge);
             Console.Write(string.Format(strAuthUrl, strClientID, strResponseType, strCodeChallenge, strCodeChallengeMethod, strScope));
-            return Redirect(string.Format(strAuthUrl, strClientID, strResponseType, strCodeChallenge, strCodeChallengeMethod, strScope)); 
+            return Redirect(string.Format(strAuthUrl, strClientID, strResponseType, strCodeChallenge, strCodeChallengeMethod, strScope));
         }
-    }
 
-    
+    }
 }
-   
